fd402e3db1b651c56d1e9262fb9cdead69d33fda
Venkatesh <tvenky359@gmail.com>
2025-11-11 20:14:53 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  type    = string
+  default = "roboshop"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+
+}
+
+variable "zone_id" {
+  default = "Z0289756D5X1LXL2LSE7"
+}
+
+variable "domain_name" {
+  default = "venkatesh.fun"
+}
